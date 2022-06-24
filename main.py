0e99b5b75c234473e4f30dcbba43294be038612f
yk <yefim_korshever@mail.ru>
2022-06-24 20:32:55 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+from datacenter.models import Passcard  # noqa: E402
+
+if __name__ == '__main__':
+    # Программируем здесь
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
---
+++ settings.py
@@ -0,0 +1,18 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '',
+        'PORT': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = 'REPLACE_ME'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
