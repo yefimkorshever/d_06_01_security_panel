3a3e75ec67444fd1c0cb399d46b1ddcc63489734
yk <yefim_korshever@mail.ru>
2022-06-25 13:37:20 +0300

    Print all visits

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit  # noqa: E402
 
 
 def main():
-    active_passcards = Passcard.objects.filter(is_active=True)
-
-    print(
-        f'Всего пропусков: {Passcard.objects.count()}',
-        f'Активных пропусков: {len(active_passcards)}',
-        sep='\n'
-    )
+    visits = Visit.objects.all()
+    print(visits)
 
 
 if __name__ == '__main__':
