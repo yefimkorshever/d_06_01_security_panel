e1dab5395fe0dd9e4f1f4e2a28843bd8ffb72b19
yk <yefim_korshever@mail.ru>
2022-06-25 11:41:48 +0300

    print a card

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ from datacenter.models import Passcard  # noqa: E402
 
 def main():
     passcards = Passcard.objects.all()
-    print(passcards)
+    print(
+        f'owner_name: {passcards[0].owner_name}',
+        f'passcode: {passcards[0].passcode}',
+        f'created_at: {passcards[0].created_at}',
+        f'is_active: {passcards[0].is_active}',
+        sep='\n'
+    )
 
 
 if __name__ == '__main__':
