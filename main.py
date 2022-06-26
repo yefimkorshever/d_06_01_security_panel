1c3c5b6c1db104943dc22af0115a60e6f39356d5
yk <yefim_korshever@mail.ru>
2022-06-26 16:02:22 +0300

    Print remaining visitors names

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from django.utils.timezone import localtime
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Visit  # noqa: E402
+from datacenter.models import Visit, Passcard  # noqa: E402
 
 
 def main():
@@ -17,16 +17,17 @@ def main():
 
     print('\n')
     for visit in remaining_visits:
-        now = localtime()
-        entry = localtime(visit.entered_at)
-        period = now - entry
-        print(
-            'Entered storage, Moscow time:',
-            entry,
-            'Remains in storage:',
-            period,
-            sep='\n'
-        )
+        print(visit.passcard)
+    #     now = localtime()
+    #     entry = localtime(visit.entered_at)
+    #     period = now - entry
+    #     print(
+    #         'Entered storage, Moscow time:',
+    #         entry,
+    #         'Remains in storage:',
+    #         period,
+    #         sep='\n'
+    #     )
 
     print('\n')
     pprint(connection.queries[0])
